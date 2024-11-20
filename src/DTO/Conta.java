6208463fd8952f187d523173868c3697de32679b
airotivmaria <mariavitoria6019@gmail.com>
2024-11-20 19:28:02 -0300

    Arquivos DTO - classes

---
+++ src/DTO/Conta.java
@@ -0,0 +1,49 @@
+package DTO;
+
+public class Conta {
+    private int numeroConta;
+    private double saldoConta;
+    Usuario usuario;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(double saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldoConta += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso!");
+        } else {
+            System.out.println("O valor do depósito deve ser positivo.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldoConta) {
+            saldoConta -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido.");
+        }
+    }
+}
---
+++ src/DTO/Usuario.java
@@ -0,0 +1,50 @@
+package DTO;
+
+public class Usuario {
+        private String nome;
+        private int idade;
+        private String cpf;
+        private String senha;
+        Conta conta;
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
+
+        public String getCpf() {
+            return cpf;
+        }
+
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Conta getConta() {
+            return conta;
+        }
+
+        public void setConta(Conta conta) {
+            this.conta = conta;
+        }
+
+}

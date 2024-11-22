5b8ad365dbc85e28886426158c2fd34a2eb1b302
airotivmaria <mariavitoria6019@gmail.com>
2024-11-21 21:24:32 -0300

    Atualização CC

--- src/DTO/ContaCorrente.java
+++ src/DTO/ContaCorrente.java
@@ -2,4 +2,19 @@ package DTO;
 
 public class ContaCorrente extends Conta{
 
+    public void aplicarRendimento() {
+        double rendimento = (int) (getSaldoConta() / 100) * 0.05;
+        setSaldoConta(getSaldoConta() + rendimento);
+        System.out.println("Rendimento de R$" + rendimento + " aplicado com sucesso!");
+    }
+
+//    public void calcularRendimentoContaCorrente() {
+//        if (getSaldoConta() >= 100) { // Só aplica rendimento se o saldo for >= 100
+//            double rendimento = (getSaldoConta() / 100) * 0.05; // 0,05% para cada 100 reais
+//            getSaldoConta() += rendimento; // Adiciona o rendimento ao saldo
+//            System.out.println("Rendimento de R$ " + rendimento + " aplicado na Conta Corrente.");
+//        } else {
+//            System.out.println("Saldo insuficiente para rendimento na Conta Corrente.");
+//        }
+//    }
 }
